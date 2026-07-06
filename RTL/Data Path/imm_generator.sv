9081e8902f41f4ccf48a0087716a639cd7fec68f
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-06 17:34:38 -0400

    Update imm_generator.sv

--- RTL/Data Path/imm_generator.sv
+++ RTL/Data Path/imm_generator.sv
@@ -2,6 +2,10 @@ import riscv_pkg::*;
 
 module imm_gen (
   input logic [31:0] instr,
-  input imm_scr,
+  input instr_fmt_e imm_scr,
   output logic [31:0] imm_out
 );
+//ISBUJ
+  always_comb
+    unique case(imm_scr)
+      
