45e4d764c934d5b4639de4c609b2a362d9668077
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-05 23:45:55 -0400

    Update instruction_mem.sv

--- RTL/Data Path/instruction_mem.sv
+++ RTL/Data Path/instruction_mem.sv
@@ -1,4 +1,10 @@
-module imem (
+module imem #(
+  parameter init_mem = ""
+)
+(
   input [31:0] pc_addr,
   output [31:0] instr
 );
+
+  logic [31:0] rom [0:1023]; //4KB memory space.
+  initial $readmemh(init_mem, rom);
