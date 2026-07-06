d6509773cde33f9cf02cc0db1fe6169ce31be6a1
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-06 17:16:15 -0400

    Update imm_generator.sv

--- RTL/Data Path/imm_generator.sv
+++ RTL/Data Path/imm_generator.sv
@@ -1,11 +1,7 @@
+import riscv_pkg::*;
+
 module imm_gen (
   input logic [31:0] instr,
   input imm_scr,
   output logic [31:0] imm_out
 );
-  
-    localparam logic [2:0] IMM_I = 3'b000;
-    localparam logic [2:0] IMM_S = 3'b001;
-    localparam logic [2:0] IMM_B = 3'b010;
-    localparam logic [2:0] IMM_U = 3'b011;
-    localparam logic [2:0] IMM_J = 3'b100;
