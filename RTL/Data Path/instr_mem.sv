e79ece3a1b794bcbcb5b383e441d1fd165aec395
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-06 16:33:28 -0400

    Update instr_mem.sv

--- RTL/Data Path/instr_mem.sv
+++ RTL/Data Path/instr_mem.sv
@@ -1,21 +1,24 @@
+import riscv_pkg::*;
+
 module imem #(
-  parameter init_mem = ""
+  parameter init_mem = "",
+  parameter int unsigned memory_space = 1024
 )
 (
-  input [31:0] pc_addr,
-  output [31:0] instr
+  input [XLEN-1:0] pc_addr,
+  output [XLEN-1:0] instr
 );
 
-  logic [31:0] rom [0:1023]; //Creates a 4KB memory space.
+  logic [XLEN-1:0] rom [0:memory_space-1]; //Creates 4KB of memory when memory_space = 1024.
   integer i;
   
   initial begin
-    for (i=0; i<1024; i++)
-      rom[i] = 32'h0000_0013; //Preemptively fills rom with NOP instructions in case of a hex file less than 1024 words.
+    for (i=0; i<memory_space; i++)
+      rom[i] = 32'h0000_0013; //Preemptively fills rom with NOP instructions in case of a hex file less than memory_space words.
     if (init_mem != "")
       $readmemh(init_mem, rom); //Overwrites NOP instructions with words from the hex file.
   end
-  
-  assign instr = rom[pc_addr[11:2]]; //Dividing values by 4 as rom is word alligned and not byte alligned.
+
+  assign instr = rom[pc_addr[$clog2(memory_space)+1:2]];  //Dividing values by 4 as rom is word alligned and not byte alligned.
   
 endmodule
