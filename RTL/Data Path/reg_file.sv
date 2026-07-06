e1efbd601408adf4610d3c894b5eebbdf6caa080
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-06 03:33:08 -0400

    Create reg_file.sv

---
+++ RTL/Data Path/reg_file.sv
@@ -0,0 +1,17 @@
+module regf (
+  input clk,
+  input [4:0] rs1, rs2, rd,
+  input [31:0] wd,
+  input we,
+  output [31:0] rdata1, rdata2
+);
+
+  logic [31:0] regs [0:31];
+
+  assign rdata1 = (rs1 == 0) ? 0 : regs[rs1];
+  assign rdata2 = (rs2 == 0) ? 0 : regs[rs2];
+
+  always @(posedge clk)
+    if (we && rd != 0)
+      regs[rd] <= wd;
+endmodule
