3d0b2b522e2d4cab0609ced0d12afa816bb666ea
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-06 16:36:48 -0400

    Update reg_file.sv

--- RTL/Data Path/reg_file.sv
+++ RTL/Data Path/reg_file.sv
@@ -2,13 +2,13 @@ import riscv_pkg::*;
 
 module regf (
   input clk,
-  input [4:0] rs1, rs2, rd,
-  input [31:0] wd,
+  input [REG_ADDER_W-1:0] rs1, rs2, rd,
+  input [XLEN-1:0] wd,
   input we,
-  output [31:0] rdata1, rdata2
+  output [XLEN-1:0] rdata1, rdata2
 );
 
-  logic [31:0] regs [0:31];
+  logic [XLEN-1:0] regs [0:31];
 
   assign rdata1 = (rs1 == 0) ? 0 : regs[rs1];
   assign rdata2 = (rs2 == 0) ? 0 : regs[rs2];
