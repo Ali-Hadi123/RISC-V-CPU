a5ad3cef45764cd5ffd7bccc565b3b06dadf0c6c
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-07 12:27:07 -0400

    Update main_decoder.sv

--- RTL/Control Unit/main_decoder.sv
+++ RTL/Control Unit/main_decoder.sv
@@ -6,7 +6,7 @@ module main_decoder (
   output [1:0] result_src,
   output mem_write,
   output alu_src,
-  output [1:0] imm_src,
+  output instr_fmt_e [1:0] imm_src,
   output reg_write,
   output [1:0] alu_op
 );
