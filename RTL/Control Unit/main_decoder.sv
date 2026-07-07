ddd728455773875790496bf7a1cc6b9548844ea8
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-07 12:25:22 -0400

    Update main_decoder.sv

--- RTL/Control Unit/main_decoder.sv
+++ RTL/Control Unit/main_decoder.sv
@@ -11,4 +11,13 @@ module main_decoder (
   output [1:0] alu_op
 );
 
+  logic branch;
+  logic jump;
   
+  always_comb begin
+    pc_src = 1'b0;
+    result_src = 1'b0;
+    mem_write = 1'b0;
+    alu_src = 1'b0;
+    imm_src = FMT_I;
+    reg_write = 1'b0;
