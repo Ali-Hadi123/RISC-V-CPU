0e9c26a5b91a73a49ec2f92c09c2d196bfbc7fd6
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-07 11:30:35 -0400

    Update main_decoder.sv

--- RTL/Control Unit/main_decoder.sv
+++ RTL/Control Unit/main_decoder.sv
@@ -1,1 +1,14 @@
 import riscv_pkg::*;
+
+module main_decoder (
+  input opcode_e opcode,
+  output pc_src,
+  output [1:0] result_src,
+  output mem_write,
+  output alu_src,
+  output [1:0] imm_src,
+  output reg_write,
+  output [1:0] alu_op
+);
+
+  
