08c4bd1cd2782ad4feeb070f3f28761c80b280d6
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-07 20:26:27 -0400

    Update data_mem.sv

--- RTL/Data Path/data_mem.sv
+++ RTL/Data Path/data_mem.sv
@@ -1,5 +1,10 @@
 import riscv_pkg::*;
 
-module dmem (
-
+module dmem #(
+  parameter int unsigned memory_space = 1024
+)
+(
+  input clk,
+  input logic [XLEN-1:0] byte_addr,
+  input logic [XLEN-1:0] wdata,
 );
