c586b3c768e92a5e1117af11b98a069c1193a79f
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-07 22:29:40 -0400

    Update data_mem.sv

--- RTL/Data Path/data_mem.sv
+++ RTL/Data Path/data_mem.sv
@@ -7,4 +7,11 @@ module dmem #(
   input clk,
   input logic [XLEN-1:0] byte_addr,
   input logic [XLEN-1:0] wdata,
+  input logic mem_read,
+  input logic mem_write,
+  input mem_size_e mem_size,
+  input logic mem_unsigned,
+  output logic [XLEN-1:0] read_data
 );
+
+  logic [XLEN-1:0] ram [0:memory_space-1]; //Creates 4KB of memory when memory_space = 1024.
