42c09dbfb91184b74ed188998b2af8f514fc6cee
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-12 18:51:16 -0400

    Update imm_generator.sv

--- RTL/Data Path/imm_generator.sv
+++ RTL/Data Path/imm_generator.sv
@@ -13,6 +13,6 @@ module imm_gen (
       FMT_B: imm_out = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
       FMT_U: imm_out = {instr[31:12], 12'b0};
       FMT_J: imm_out = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      default: imm_out {XLEN{1'b0}};
+      default: imm_out = {XLEN{1'b0}};
     endcase
 endmodule
