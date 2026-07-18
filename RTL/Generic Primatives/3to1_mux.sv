789d1d2247aa1edd7e2eb020360aa687fad8c54f
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-18 12:01:27 -0400

    Create 3to1_mux.sv

---
+++ RTL/Generic Primatives/3to1_mux.sv
@@ -0,0 +1,15 @@
+module mux3 #(parameter WIDTH = 32) (
+  input logic [WIDTH-1:0] a, b, c,
+  input logic [1:0] sel,
+  output logic [WIDTH-1:0] result
+);
+
+  always_comb begin
+    unique case(sel)
+      2'b00: result = a;
+      2'b01: result = b;
+      2'b10: result = c;
+    endcase
+  end
+
+endmodule
