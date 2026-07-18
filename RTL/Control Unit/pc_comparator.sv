5995a686d239605042638bad4f66ccc53d7218b7
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-18 19:59:43 -0400

    Update pc_comparator.sv

--- RTL/Control Unit/pc_comparator.sv
+++ RTL/Control Unit/pc_comparator.sv
@@ -2,7 +2,7 @@ module pc_comp (
   input logic branch,
   input logic branch_taken,
   input logic jump,
-  output logic pc_src
+  output pc_src_e pc_src
 );
 
   assign pc_src = jump | (branch & branch_taken);
