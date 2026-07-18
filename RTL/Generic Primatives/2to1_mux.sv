9879c9ec4692204af7fbc4898eef0225f018395d
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-18 11:54:37 -0400

    Add parameterized 2-to-1 multiplexer module

--- RTL/Generic Primatives/2to1_mux.sv
+++ RTL/Generic Primatives/2to1_mux.sv
@@ -1,1 +1,9 @@
-module mux2 #(parameter WIDTH
+module mux2 #(parameter WIDTH = 32)(
+  input logic [WIDTH-1:0] a, b,
+  input logic sel,
+  output logic [WIDTH-1:0] result
+);
+
+  assign result = sel ? a : b;
+
+endmodule
