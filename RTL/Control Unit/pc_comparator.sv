f8765ef37fc0af8d246d8cf775c710bfa7e62690
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-19 00:36:06 -0400

    Update pc_comparator.sv

--- RTL/Control Unit/pc_comparator.sv
+++ RTL/Control Unit/pc_comparator.sv
@@ -1,12 +1,20 @@
 import riscv_pkg::*;
 
 module pc_comp (
-  input logic branch,
+  input logic is_branch,
   input logic branch_taken,
-  input logic jump,
+  input logic is_jal,
+  input logic is_jalr,
   output pc_src_e pc_src
 );
 
-  assign pc_src = jump | (branch & branch_taken);
+  always_comb begin
+    if (is_jal && is_jalr)
+      pc_src = PC_RESULT;
+    else if (is_jal || (is_branch & branch_taken))
+      pc_src = PC_TARGET;
+    else
+      pc_src = PC_PLUS4;
+  end
   
 endmodule
