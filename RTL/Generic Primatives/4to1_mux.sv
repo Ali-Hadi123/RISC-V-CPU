2891efc86182988e89be2747355131c53012f7fe
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-19 09:57:32 -0400

    Create 4to1_mux.sv

---
+++ RTL/Generic Primatives/4to1_mux.sv
@@ -0,0 +1,16 @@
+module mux4 #(parameter WIDTH = 32) (
+  input logic [WIDTH-1:0] a, b, c, d,
+  input logic [1:0] sel,
+  output logic [WIDTH-1:0] result
+);
+
+  always_comb begin
+    unique case(sel)
+      2'b00: result = a;
+      2'b01: result = b;
+      2'b10: result = c;
+      2'b11: result = d;
+    endcase
+  end
+
+endmodule
