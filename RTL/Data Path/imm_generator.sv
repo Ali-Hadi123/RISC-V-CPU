9d24cff624dd90d42e2dfe012b43e9d7205abc55
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-19 15:54:29 -0400

    Update imm_generator.sv

--- RTL/Data Path/imm_generator.sv
+++ RTL/Data Path/imm_generator.sv
@@ -1,4 +1,4 @@
-import riscv_pkg::*;
+//imm_gen is hardcoded for RV32I since different instruction sets (e.g. RV64I) would require different splicing.
 
 module imm_gen (
   input logic [31:0] instr,
@@ -6,13 +6,15 @@ module imm_gen (
   output logic [31:0] imm_out
 );
 
-  always_comb            //Obtaining the immediate from the instruction differently based on what type of instruction imm_scr reports.
+  //Obtaining the immediate from the instruction differently based on what type of instruction imm_scr reports.
+  
+  always_comb            
     unique case(imm_scr)  
       FMT_I: imm_out = {{20{instr[31]}}, instr[31:20]};
       FMT_S: imm_out = {{20{instr[31]}}, instr[31:25], instr[11:7]};
       FMT_B: imm_out = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
       FMT_U: imm_out = {instr[31:12], 12'b0};
       FMT_J: imm_out = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      default: imm_out = {XLEN{1'b0}};
+      default: imm_out = {32{1'b0}};
     endcase
 endmodule
