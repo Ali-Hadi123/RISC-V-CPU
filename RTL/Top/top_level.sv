b2eec49f1881e28a5d412be19fedd4209bde482b
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-19 22:12:03 -0400

    Update top_level.sv

--- RTL/Top/top_level.sv
+++ RTL/Top/top_level.sv
@@ -9,26 +9,48 @@ module top #(
   input logic rst
 );
 
-  //Initializations: 
-  
-  //pc:
-  logic [XLEN-1:0] pc;
-  logic [XLEN-1:0] next_pc;
-  logic [XLEN-1:0] PCPlus4;
-  logic [XLEN-1:0] PCTarget;
-  logic [XLEN-1:0] PCResult;
+  //Fetching Instructions:
 
-  //instr:
-  logic [ILEN-1:0] instr;
+  logic [XLEN-1] pc_out;
+  logic [XLEN-1] next_pc;
+  logic [XLEN-1] instr;
 
-  //Control Signals:
+  pc u_pc(
+    .clk(clk),
+    .rst(rst),
+    .next_pc(next_pc),
+    .pc_out(pc_out)
+  );
+
+  imem #(
+    .init_mem(init_mem),
+    .rom_size(rom_size)
+  ) u_imem(
+    .pc_addr(pc_out),
+    .instr(instr)
+  );
+
+  //Instruction Field Extraction:
+
+  opcode_e op_code;
+  logic [4:0] rs1_addr, rs2_addr, rd_addr;
+  logic [2:0] funct3;
+  logic [6:0] funct7;
+
+  assign op_code = opcode_e'(get_opcode(instr));
+  assign rs1_addr = get_rs1(instr);
+  assign rs2_addr = get_rs2(instr);
+  assign rd_addr = get_rd(instr);
+  assign funct3 = get_funct3(instr);
+  assign funct7 = get_funct7(instr);
+
+  //Generating Control Signals:
+
   result_src_e result_src;
-  logic mem_read;
-  logic mem_write;
+  logic mem_read, mem_write;
   alu_src_e alu_src;
   instr_fmt_e imm_src;
   logic reg_write;
-  alu_ctrl_e alu_ctrl;
-  mem_size_e mem_size;
-  logic mem_unsigned;
-  pc_src_e pc_src;
+  logic is_branch, is_jal, is_jalr;
+  alu_op_e alu_op;
+  logic illegal_instr_main;
