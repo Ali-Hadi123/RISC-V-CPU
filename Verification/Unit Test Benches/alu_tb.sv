27e0511c0b1c111bbef3405fa08458008162111e
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-21 15:01:03 -0400

    Update alu_tb.sv

--- Verification/Unit Test Benches/alu_tb.sv
+++ Verification/Unit Test Benches/alu_tb.sv
@@ -1,2 +1,71 @@
 import riscv_pkg::*;
+`timescale 1ns/1ps
 
+module alu_tb;
+
+  logic [XLEN-1:0] data_a, data_b;
+  logic alu_ctrl_e tb_alu_ctrl;
+  logic [XLEN-1:0] tb_result;
+  logic tb_zero, tb_less, tb_less_u;
+
+  //Counters:
+  int unsigned total_tests = 0;
+  int unsigned passed_tests = 0;
+  
+  alu duv(
+    .alu_ctrl(tb_alu_ctrl),
+    .a(data_a),
+    .b(data_b),
+    .result(tb_result),
+    .is_zero(tb_zero),
+    .is_less(tb_less),
+    .is_less_u(tb_less_u)
+  );
+
+  task verify_alu(
+    input logic [XLEN-1:0] a,
+    input logic [XLEN-1:0] b,
+    input alu_ctrl_e ctrl,
+    input logic [XLEN-1:0] exp_result,
+    input logic exp_zero,
+    input logic exp_less,
+    input logic exp_less_u,
+    input string test_name
+  );
+
+    data_a = a;
+    data_b = b;
+    tb_alu_ctrl = ctrl;
+
+    #10;
+
+    total_tests++;
+
+    if (tb_result === exp_result && tb_zero === exp_zero && tb_less === exp_less && tb_less_u === exp_less_u) begin
+      passed_tests++;
+      $display("Passed: %s", test_name);
+    end
+    else
+      $display("Failed: %s", test_name);
+  endtask
+  
+  initial begin
+    $dumpfile("alu_tb.vcd");
+    $dumpvars(0, alu_tb);
+
+    data_a = 32'd0;    //Ensuring all signals are cleared before testing.
+    data_b = 32'd0;
+    alu_ctrl = 4'b0;
+
+    #10;
+
+    $display("STARTING ALU TESTING");
+
+    verify_alu(32'd56, 32'd44, ALU_ADD, 32'd100, 1'b0, 1'b0, 1'b0, "Test 1: Addition") //Testing 56 + 44 = 100
+    verify_alu(32'd1, 32'd53, ALU_SUB, -32'd52, 1'b0, 1'b1, 1'b1, "Test 2: Subtraction") //Testing 1 - 53 = -52
+    verify_alu(32'b10111, 32'b10101, ALU_AND, 32'b10101, 1'b0, 1'b0, 1'b0, "Test 3: AND") //Testing 10111 & 10101 = 10101
+    verify_alu(32'b0011, 32'b1011, ALU_OR, 32'b1011, 1'b0, 1'b0, 1'b1, "Test 4: OR") //Testing 0011 | 1011 = 1011
+    verify_alu(32'b111, 32'b101, ALU_XOR, 32'b010, 1'b0, 1'b0, 1'b0, "Test 5: XOR") //Testing 111 ^ 101 = 010
+    verify_alu(32'd5, -32'd10, ALU_SLT, 32'd0, 1'b0, 1'b0, 1'b1, "Test 6: SLT") //Testing 5 < -10 = False
+    verify_alu(32'd5, -32'd10, ALU_SLTU, 32'd1, 1'b0, 1'b0, 1'b1, "Test 7: SLTU") //Testing 5 < -10 = True (unsigned)
+    verify_alu(32'd5, -32'd10, ALU_SLT, 32'd0, 1'b0, 1'b0, 1'b1, "Test 6: SLT") //Testing 5 < -10 = False
