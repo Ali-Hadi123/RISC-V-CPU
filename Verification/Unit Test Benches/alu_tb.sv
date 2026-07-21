cfb84958e0ef4a784a1df7e326df30db73585489
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-21 19:54:46 -0400

    Update alu_tb.sv

--- Verification/Unit Test Benches/alu_tb.sv
+++ Verification/Unit Test Benches/alu_tb.sv
@@ -46,7 +46,10 @@ module alu_tb;
       $display("Passed: %s", test_name);
     end
     else
-      $display("Failed: %s", test_name);
+      $display(
+        "FAILED: %s\nExpected result=%h zero=%b less=%b less_u=%b\nGot result=%h zero=%b less=%b less_u=%b",
+        test_name, exp_result, exp_zero, exp_less, exp_less_u, tb_result, tb_zero, tb_less, tb_less_u
+      );
   endtask
   
   initial begin
@@ -64,7 +67,7 @@ module alu_tb;
     verify_alu(32'd56, 32'd44, ALU_ADD, 32'd100, 1'b0, 1'b0, 1'b0, "Test 1: Addition"); //Testing 56 + 44 = 100
     verify_alu(32'd1, 32'd53, ALU_SUB, -32'd52, 1'b0, 1'b1, 1'b1, "Test 2: Subtraction"); //Testing 1 - 53 = -52
     verify_alu(32'b10111, 32'b10101, ALU_AND, 32'b10101, 1'b0, 1'b0, 1'b0, "Test 3: AND"); //Testing 10111 & 10101 = 10101
-    verify_alu(32'b0011, 32'b1011, ALU_OR, 32'b1011, 1'b0, 1'b0, 1'b1, "Test 4: OR"); //Testing 0011 | 1011 = 1011
+    verify_alu(32'b0011, 32'b1011, ALU_OR, 32'b1011, 1'b0, 1'b0, 1'b0, "Test 4: OR"); //Testing 0011 | 1011 = 1011
     verify_alu(32'b111, 32'b101, ALU_XOR, 32'b010, 1'b0, 1'b0, 1'b0, "Test 5: XOR"); //Testing 111 ^ 101 = 010
     verify_alu(32'd5, -32'd10, ALU_SLT, 32'd0, 1'b0, 1'b0, 1'b1, "Test 6: SLT"); //Testing 5 < -10 = False
     verify_alu(32'd5, -32'd10, ALU_SLTU, 32'd1, 1'b0, 1'b0, 1'b1, "Test 7: SLTU"); //Testing 5 < -10 = True (unsigned)
