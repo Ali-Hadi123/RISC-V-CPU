8d7c00cf3981c700f39a25abe611680a120ab38c
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-23 16:55:03 -0400

    Update reg_file_tb.sv

--- Verification/Unit Test Benches/reg_file_tb.sv
+++ Verification/Unit Test Benches/reg_file_tb.sv
@@ -26,26 +26,22 @@ module regf_tb;
 
   //Concurrent assert statements for testing (these tests are constantly being ran):
   
-  property x0_value:
+  property x0_value;
     @(posedge clk) (tb_rs1 == 5'b0) |-> (tb_rdata1 == 32'b0);
   endproperty
-
-  total_tests++;
   
-  assert x0_value begin
+  assert property(x0_value) begin
     passed_tests++;
     $display("x0 holds 0 is true, test PASSED.");
   end
   else
     $error("x0 does NOT hold 0, test FAILED.");
 
-  property write_then_read:
-    @(posedge clk) (reg_write && tb_rs1 != 0 && tb_rs1 == tb_rd) |=> (tb_rdata1 == $past(wd));
+  property write_then_read;
+    @(posedge clk) (reg_write && tb_rs1 != 0 && tb_rs1 == tb_rd) |=> (tb_rdata1 == $past(tb_wd));
   endproperty
 
-  total_tests++;
-
-  assert write_then_read begin
+  assert property(write_then_read) begin
     passed_tests++;
     $display("Writing and then reading a register works, test PASSED.");
   end
@@ -72,20 +68,23 @@ module regf_tb;
 
   initial begin
     $dumpfile("reg_file_tb.vcd");
-    $dumpvars(0, reg_file_tb);
+    $dumpvars(0, regf_tb);
     
     clk = 0;
     reg_write = 0;
-    rs1 = 0;
-    rs2 = 0;
-    rd = 0;
-    wd = 0;
+    tb_rs1 = 0;
+    tb_rs2 = 0;
+    tb_rd = 0;
+    tb_wd = 0;
 
-    $display("STARTING REGISTER FILE TESTING:")
+    $display("STARTING REGISTER FILE TESTING:");
 
     write_reg(5'd5, 32'd123);              //Testing writing a value to x5 and then reading it.
     read_reg(5'd5);
     total_tests++;
+
+    #10;
+    
     assert (tb_rdata1 == 32'd123) begin
       passed_tests++;
       $display("Passed Test 1");
@@ -96,6 +95,9 @@ module regf_tb;
     write_reg(5'd8, 32'd456);       //Testing that different registers can be written to w/o overwriting data.
     read_reg(5'd5);
     total_tests++;
+
+    #10;
+    
     assert (tb_rdata1 == 32'd123) begin
       passed_tests++;
       $display("Passed Test 1");
@@ -105,6 +107,9 @@ module regf_tb;
 
     read_reg(5'd8);                    //Testing reading from a different register.
     total_tests++;
+
+    #10;
+    
     assert (tb_rdata1 == 32'd456) begin
       passed_tests++;
       $display("Passed Test 1");
@@ -115,15 +120,19 @@ module regf_tb;
     write_reg(5'd0, 32'hFFFF_FFFF);      //Testing that x0 can't be overwritten.
     read_reg(5'd0);
     total_tests++;
-    assert (tb_rdata1 == 32'0) begin
+
+    #10;
+    
+    assert (tb_rdata1 == 32'd0) begin
       passed_tests++;
       $display("Passed Test 4");
     end
     else
-      $error("Failed Test 4\nExpected: 32'0\nGot: %0d", tb_rdata1);
+      $error("Failed Test 4\nExpected: 32'd0\nGot: %0d", tb_rdata1);
 
     //Summary
     $display("REGFILE TESTING COMPLETE!");
     $display("Results: %0d/%0d tests passed.", passed_tests, total_tests);
   end
+  $finish;
 endmodule
